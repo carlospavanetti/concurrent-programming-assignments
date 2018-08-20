e821e133c2066c388d93e2783c5c4ad35a55b5ef
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-08-19 21:41:21 -0300

    Parallels distances computation
    It creates N pthreads based on -thread runtime argument to compute
    distances per blocks.

--- radial_distance/pthreads/main.c
+++ radial_distance/pthreads/main.c
@@ -21,7 +21,7 @@ void read_arguments(int argc, char **argv, size_t *size, int *threads);
 void initialize_with_random(RadialPoints *, int size);
 void release_points(RadialPoints *);
 void release_distances(double *);
-double *points_distances(RadialPoints *, RadialPoints *);
+double *points_distances(RadialPoints *, RadialPoints *, int threads);
 
 int main(int argc, char **argv) {
   size_t size;
@@ -33,7 +33,7 @@ int main(int argc, char **argv) {
   initialize_with_random(&points[0], size);
   initialize_with_random(&points[1], size);
 
-  double *distances = points_distances(&points[0], &points[1]);
+  double *distances = points_distances(&points[0], &points[1], threads);
 
   release_points(&points[0]);
   release_points(&points[1]);
@@ -89,10 +89,46 @@ void compute_distances_block(
   }
 }
 
-double *points_distances(RadialPoints *A, RadialPoints *B) {
+typedef struct {
+  RadialPoints *first_set;
+  RadialPoints *second_set;
+  double *distances;
+  int block_offset;
+  int block_size;
+} RDArguments;
+
+void *radial_distances_process(void *amorph) {
+  RDArguments *args = (RDArguments *) amorph;
+  compute_distances_block(
+    args->first_set, args->second_set, args->distances,
+    args->block_offset, args->block_size
+  );
+  pthread_exit(NULL);
+  return NULL;
+}
+
+double *points_distances(RadialPoints *A, RadialPoints *B, int threads) {
   size_t size = min(A->size, B->size);
   double *distances = (double *) malloc(size * sizeof(double));
-  compute_distances_block(A, B, distances, 0, size);
+  if (threads <= 1) {
+    compute_distances_block(A, B, distances, 0, size);
+  } else {
+    pthread_t *workers = (pthread_t *) malloc(threads * sizeof(pthread_t));
+    int block_size = (int) (size / min(size, threads));
+    for (int i = 0; i < threads; i++) {
+      RDArguments *arguments = (RDArguments *) malloc(sizeof(RDArguments));
+      arguments->first_set  = A;
+      arguments->second_set = B;
+      arguments->distances = distances;
+      arguments->block_offset = min(block_size * i, size - 1);
+      arguments->block_size = min(block_size, size);
+      pthread_create(&workers[i], NULL, radial_distances_process, arguments);
+    }
+    for (int i = 0; i < threads; i++) {
+      pthread_join(workers[i], NULL);
+    }
+    free(workers);
+  }
   return distances;
 }
 
