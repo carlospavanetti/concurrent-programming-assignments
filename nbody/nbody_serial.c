ef9f75bfcdd27a828179886a043e0c9dd44a471b
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-08-25 22:18:07 -0300

    Format nbody original code
    Apply cosmetics changes with spaces, tabs, lines and declarations.

--- nbody/nbody_serial.c
+++ nbody/nbody_serial.c
@@ -13,13 +13,12 @@
 
 static long seed = DEFAULT;
 
-double Random(void)
 /* ----------------------------------------------------------------
  * Random returns a pseudo-random real number uniformly distributed
  * between 0.0 and 1.0.
  * ----------------------------------------------------------------
  */
-{
+double Random(void) {
   const long Q = MODULUS / MULTIPLIER;
   const long R = MODULUS % MULTIPLIER;
   long t;
@@ -40,94 +39,86 @@ typedef struct {
   double x, y, z;
   double mass;
 } Particle;
+
 typedef struct {
   double xold, yold, zold;
   double fx, fy, fz;
 } ParticleV;
 
-void InitParticles( Particle[], ParticleV [], int );
-double ComputeForces( Particle [], Particle [], ParticleV [], int );
-double ComputeNewPos( Particle [], ParticleV [], int, double);
+void InitParticles(Particle[], ParticleV [], int);
+double ComputeForces(Particle [], Particle [], ParticleV [], int);
+double ComputeNewPositions(Particle [], ParticleV [], int, double);
 
-int main()
-{
-  //double time;
+int main() {
   Particle  * particles;   /* Particulas */
   ParticleV * pv;          /* Velocidade da Particula */
   int         npart;
   double      sim_t;       /* Tempo de Simulacao */
 
-  scanf("%d",&npart);
+  scanf("%d", &npart);
   //npart = 25000;
   /* Allocate memory for particles */
-  particles = (Particle *) malloc(sizeof(Particle)*npart);
-  pv = (ParticleV *) malloc(sizeof(ParticleV)*npart);
+  particles = (Particle *) malloc(
+    npart * sizeof(Particle)
+  );
+  pv = (ParticleV *) malloc(
+    npart * sizeof(ParticleV)
+  );
   /* Generate the initial values */
-  InitParticles( particles, pv, npart);
-
-  sim_t = 0.0;
+  InitParticles(particles, pv, npart);
 
   struct timeval inicio, final2;
   int tmili;
-
-  int tid;
-  double max_f;
-
   gettimeofday(&inicio, NULL);
 
+  sim_t = 0.0;
+  double max_f;
   /* Compute forces (2D only) */
   max_f = ComputeForces(particles, particles, pv, npart);
   /* Once we have the forces, we compute the changes in position */
-  sim_t = ComputeNewPos(particles, pv, npart, max_f);
+  sim_t = ComputeNewPositions(particles, pv, npart, max_f);
 
   gettimeofday(&final2, NULL);
-  tmili = (int) (1000 * (final2.tv_sec - inicio.tv_sec) + 
-		 (final2.tv_usec - inicio.tv_usec) / 1000);
-
-  //for (i=0; i<npart; i++)
-  //fprintf(stdout,"%.5lf %.5lf %.5lf\n", particles[i].x, particles[i].y, particles[i].z);
+  tmili = (int) (
+    1000 * (final2.tv_sec - inicio.tv_sec) +
+    (final2.tv_usec - inicio.tv_usec) / 1000
+  );
 
   printf("%g\n", max_f);
   printf("%g\n", sim_t);
-
   printf("%d\n", tmili);
 
   return 0;
 }
 
-void InitParticles( Particle particles[], ParticleV pv[], int npart )
-{
-  int i;
-  for (i=0; i<npart; i++) {
-    particles[i].x	  = Random();
-    particles[i].y	  = Random();
-    particles[i].z	  = Random();
+void InitParticles(Particle particles[], ParticleV pv[], int npart) {
+  for (int i = 0; i < npart; i++) {
+    particles[i].x    = Random();
+    particles[i].y    = Random();
+    particles[i].z    = Random();
     particles[i].mass = 1.0;
-    pv[i].xold	  = particles[i].x;
-    pv[i].yold	  = particles[i].y;
-    pv[i].zold	  = particles[i].z;
-    pv[i].fx	  = 0;
-    pv[i].fy	  = 0;
-    pv[i].fz	  = 0;
+    pv[i].xold        = particles[i].x;
+    pv[i].yold        = particles[i].y;
+    pv[i].zold        = particles[i].z;
+    pv[i].fx          = 0;
+    pv[i].fy          = 0;
+    pv[i].fz          = 0;
   }
 }
 
-double ComputeForces( Particle myparticles[], Particle others[], ParticleV pv[], int npart )
-{
-  double max_f;
-  int i;
-  max_f = 0.0;
-
-  int j;
+double ComputeForces(
+  Particle myparticles[], Particle others[], ParticleV pv[], int npart
+) {
+  double max_f = 0.0;
   double xi, yi, rx, ry, mj, r, fx, fy, rmin;
 
-  for (i=0; i<npart; i++) {
+  for (int i = 0; i < npart; i++) {
     rmin = 100.0;
     xi   = myparticles[i].x;
     yi   = myparticles[i].y;
     fx   = 0.0;
     fy   = 0.0;
-    for (j=0; j<npart; j++) {
+    for (int j = 0; j < npart; j++) {
       rx = xi - others[j].x;
       ry = yi - others[j].y;
       mj = others[j].mass;
@@ -141,26 +132,26 @@ double ComputeForces( Particle myparticles[], Particle others[], ParticleV pv[],
     }
     pv[i].fx += fx;
     pv[i].fy += fy;
-    fx = sqrt(fx*fx + fy*fy)/rmin;
+    fx = sqrt(fx * fx + fy * fy) / rmin;
     if (fx > max_f) max_f = fx;
   }
   return max_f;
 }
 
-double ComputeNewPos( Particle particles[], ParticleV pv[], int npart, double max_f)
-{
-  int i;
+double ComputeNewPositions(
+  Particle particles[], ParticleV pv[], int npart, double max_f
+) {
   double a0, a1, a2;
   static double dt_old = 0.001, dt = 0.001;
   double dt_new;
-  a0	 = 2.0 / (dt * (dt + dt_old));
-  a2	 = 2.0 / (dt_old * (dt + dt_old));
-  a1	 = -(a0 + a2);
+  a0 = 2.0 / (dt * (dt + dt_old));
+  a2 = 2.0 / (dt_old * (dt + dt_old));
+  a1 = -(a0 + a2);
 
-  for (i=0; i<npart; i++) {
+  for (int i = 0; i < npart; i++) {
     double xi, yi;
-    xi	           = particles[i].x;
-    yi	           = particles[i].y;
+    xi             = particles[i].x;
+    yi             = particles[i].y;
     particles[i].x = (pv[i].fx - a1 * xi - a2 * pv[i].xold) / a0;
     particles[i].y = (pv[i].fy - a1 * yi - a2 * pv[i].yold) / a0;
     pv[i].xold     = xi;
@@ -168,7 +159,7 @@ double ComputeNewPos( Particle particles[], ParticleV pv[], int npart, double ma
     pv[i].fx       = 0;
     pv[i].fy       = 0;
   }
-  dt_new = 1.0/sqrt(max_f);
+  dt_new = 1.0 / sqrt(max_f);
   /* Set a minimum: */
   if (dt_new < 1.0e-6) dt_new = 1.0e-6;
   /* Modify time step */
@@ -182,4 +173,3 @@ double ComputeNewPos( Particle particles[], ParticleV pv[], int npart, double ma
   }
   return dt_old;
 }
-
