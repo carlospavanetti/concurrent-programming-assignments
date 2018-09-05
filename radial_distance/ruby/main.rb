2df660c5cc8f8fd356d29ae279081b9567831bdb
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-09-05 13:09:25 -0300

    Add measure of time to calculate
    It uses default ruby libray `benchmark`.

--- radial_distance/ruby/main.rb
+++ radial_distance/ruby/main.rb
@@ -3,9 +3,17 @@ require './polar_point'
 require './radial_distances.rb'
 require './maximum_of.rb'
 
+require 'benchmark'
+
 arguments  = RadialDistancesArguments.new(ARGV)
 generator  = Factory::RandomPoint.new(100)
 references = Collection::PolarPoints.new(arguments.size(), generator)
 targets    = Collection::PolarPoints.new(arguments.size(), generator)
 distances  = Collection::Serial::Distances.new(references, targets)
-maximumOf  = Reductor::Serial::MaximumOf.new(distances)
+maximum_of = Reductor::Serial::MaximumOf.new(distances)
+
+if arguments.maximum()
+  puts(Benchmark.measure {maximum_of.value()})
+else
+  puts(Benchmark.measure {distances.value()})
+end
